5dc3b8f293be1ea2040f2ccd8b2910bbd79e0d78
AugustHoeg <aulho@dtu.dk>
2025-06-23 09:49:19 +0200

    'update'

--- project/speed_tests.py
+++ project/speed_tests.py
@@ -188,7 +188,7 @@ def plot_time_plots_multi(result_dict_list, save_path=None, filename_prefix='plo
         plt.plot(data['result_dict']['sliding_window_avg'][:max_iterations])
     plt.xlabel('Iteration', fontsize=18)
     plt.ylabel('Time [s]', fontsize=18)
-    plt.title(f"Iteration/sec, patch size: ${data['patch_size']}^3$", fontsize=20)
+    plt.title(f"Time per iteration, patch size: ${data['patch_size']}^3$", fontsize=20)
     plt.ylim(0, np.max(data['result_dict']['sliding_window_avg']) * 1.1)
     plt.legend([f"Chunks: ${data['chunk_size']}^3$" for data in result_dict_list], fontsize=14)
     plt.grid(True)
